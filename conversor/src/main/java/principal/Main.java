c0aabd7a8d558a249b4cfca5ce2eae5168a8924b
CRICAPOGA <camilochoposada2@gmail.com>
2025-01-13 15:23:31 -0500

    Create an HTTP client to make requests to the currency API.

--- conversor/src/main/java/principal/Main.java
+++ conversor/src/main/java/principal/Main.java
@@ -1,6 +1,28 @@
 package principal;
 
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+
 public class Main {
     public static void main(String[] args) {
+        try {
+            HttpClient client = HttpClient.newHttpClient();
+            HttpRequest request = HttpRequest.newBuilder()
+                    .uri(URI.create("https://latest.currency-api.pages.dev/v1/currencies/usd.json"))
+                    .GET()
+                    .build();
+            HttpResponse<String> response = client
+                    .send(request, HttpResponse.BodyHandlers.ofString());
+
+            System.out.println("Estado: " + response.statusCode());
+            System.out.println("Cuerpo: " + response.body());
+
+        } catch (Exception e) {
+            // e.printStackTrace();
+            System.out.println("Hubo un Error: " + e.getMessage());
+        }
     }
+    
 }
